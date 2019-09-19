dce1fb95ebaaeb01237aa47c95eba361d0da99a2
Adam Hamden <ahamden@usc.edu>
2019-09-19 11:55:12 -0700

    Implement AWS connection

--- aws-sql-logger/sql_logger.py
+++ aws-sql-logger/sql_logger.py
@@ -1,20 +1,15 @@
-# !/usr/bin/env python3
-
 import yaml
 import mysql.connector
-
+import time
 
 class SQLLogger:
 
     def __init__(self):
 
-        print("1")
         self.cfg = None
         with open("config.yml", 'r') as ymlfile:
-            print("2")
             self.cfg = yaml.safe_load(ymlfile)
 
-        print("can open")
 
         self.db = mysql.connector.connect(
           host= self.cfg["sql_database"]["host"],
@@ -28,12 +23,24 @@ class SQLLogger:
         self.database_name = self.cfg["log_info"]["database_name"]
         self.keepLocalCopy = self.cfg["log_info"]["keep_local_copy"]
 
-        self.cursor = db.cursor()
+        self.cursor = self.db.cursor(prepared=True)
 
-        self.cursor.execute("CREATE DATABASE IF NOT EXISTS ?", (self.database_name,))
-        self.cursor.execute("USE ?", (self.database_name,))
+        self.cursor.execute("CREATE DATABASE IF NOT EXISTS  "+ self.database_name)
 
+        self.cursor.execute("SHOW DATABASES")
 
+        for x in self.cursor:
+            print(x)
+
+        self.db = mysql.connector.connect(
+            host=self.cfg["sql_database"]["host"],
+            port=self.cfg["sql_database"]["port"],
+            user=self.cfg["sql_database"]["user"],
+            passwd=self.cfg["sql_database"]["password"],
+            database= self.cfg["log_info"]["database_name"],
+
+        )
+        self.cursor = self.db.cursor(prepared=True)
 
         self.cursor.execute("CREATE TABLE IF NOT EXISTS " +
                             "log(" +
@@ -45,21 +52,22 @@ class SQLLogger:
                             ")"
                             )
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS " +
-                            "local_log(" +
-                            "timestamp TEXT NOT NULL, " +
-                            "topic_id TEXT NOT NULL, " +
-                            "data BLOB NOT NULL, " +
-                            "source TEXT NOT NULL, " +
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS " 
+                            "local_log(" 
+                            "timestamp TEXT NOT NULL, " 
+                            "topic_id TEXT NOT NULL, " 
+                            "data BLOB NOT NULL, "
+                            "source TEXT NOT NULL, "
                             "mismatched BOOLEAN"
                             ")"
                             )
 
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS " +
-                            "topics(" +
-                            "topic_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                            "topic_name TEXT NOT NULL, " +
-                            "data_type TEXT NOT NULL" +
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS "
+                            "topics(" 
+                            "topic_id INT AUTO_INCREMENT, " 
+                            "topic_name TEXT NOT NULL, " 
+                            "data_type TEXT NOT NULL, "
+                            "PRIMARY KEY (topic_id)"
                             ")"
                             )
 
@@ -67,6 +75,13 @@ class SQLLogger:
 
         for x in self.cursor:
             print(x)
+
+
+        self.cursor.execute("DROP TABLE log")
+        self.cursor.execute("DROP TABLE local_log")
+        self.cursor.execute("DROP TABLE topics")
+
+        time.sleep(200)
 
 
 if __name__ == "__main__":
